a0df544be22b90fe9cd58b56ed33113e9f258683
TecAbel <abelardo96.work@gmail.com>
2026-04-13 13:14:19 -0600

    feat(crud): ok

--- api/src/main/java/com/tecabel/tecabel_interview/entities/Person.java
+++ api/src/main/java/com/tecabel/tecabel_interview/entities/Person.java
@@ -9,12 +9,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "person")
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class Person {
 
---
+++ api/src/main/java/com/tecabel/tecabel_interview/services/PersonService.java
@@ -0,0 +1,59 @@
+package com.tecabel.tecabel_interview.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.tecabel.tecabel_interview.dtos.person.PersonRequestDto;
+import com.tecabel.tecabel_interview.entities.Person;
+import com.tecabel.tecabel_interview.repositories.PersonRepository;
+
+@Service
+public class PersonService {
+
+  @Autowired
+  private PersonRepository personRepository;
+
+  public List<Person> retrieve() {
+    return this.personRepository.findAll();
+  }
+
+  public Person findById(Integer id) {
+    var person = this.personRepository.findById(id);
+    if (person.get() == null)
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuario no encontrado");
+    return person.get();
+  }
+
+  public Person save(PersonRequestDto requestDto) {
+    Person person = new Person();
+
+    person.setName(requestDto.getName());
+    person.setLastName(requestDto.getLastName());
+    person.setBirthday(requestDto.getBirthday());
+    person.setJobPosition(requestDto.getJobPosition());
+    person.setSalary(requestDto.getSalary());
+
+    return this.personRepository.save(person);
+  }
+
+  public Person update(Integer id, PersonRequestDto requestDto) {
+    Person person = this.findById(id);
+
+    person.setName(requestDto.getName());
+    person.setLastName(requestDto.getLastName());
+    person.setBirthday(requestDto.getBirthday());
+    person.setJobPosition(requestDto.getJobPosition());
+    person.setSalary(requestDto.getSalary());
+
+    return this.personRepository.save(person);
+  }
+
+  public void deleteById(Integer id) {
+    this.personRepository.deleteById(id);
+  }
+}
